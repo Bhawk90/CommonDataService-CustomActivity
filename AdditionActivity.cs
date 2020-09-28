fc925eede33d8603c5e18bc3c7c42fb80a46cf53
Gábor GULYÁS <gulyas.g@outlook.com>
2020-09-28 20:55:12 +0200

    Initial code for Custom Activity

---
+++ AdditionActivity.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
+
+namespace CDS.CustomActivities
+{
+    /// <summary>
+    /// This Custom Activity can be used to add to numbers to each other.
+    /// </summary>
+    public sealed class AdditionActivity : CodeActivity
+    {
+        #region Arguments
+
+        [Input("Operand A")]
+        public InArgument<int> OperandA { get; set; }
+
+        [Input("Operand B")]
+        public InArgument<int> OperandB { get; set; }
+
+        [Output("Result")]
+        public OutArgument<int> Result { get; set; }
+
+        #endregion Arguments
+
+        #region Activity Logic
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            int a = OperandA.Get<int>(executionContext);
+            int b = OperandB.Get<int>(executionContext);
+
+            tracingService.Trace($"Adding '{a}' to '{b}'.");
+
+            Result.Set(executionContext, a + b);
+
+            tracingService.Trace($"Result set to '{a + b}'.");
+        }
+
+        #endregion Activity Logic
+    }
+}
